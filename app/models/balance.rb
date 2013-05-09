a4283ccc0d8298fc327913c16061acd4a60b9d36
Sherlock Yao <yaosherlock@gmail.com>
2013-05-09 17:24:39 +0800

    added default balance value to dashboard page

--- app/models/balance.rb
+++ app/models/balance.rb
@@ -1,4 +1,9 @@
 class Balance < ActiveRecord::Base
   belongs_to :user
   attr_accessible :credit, :debit
+
+  after_initialize do
+    self.credit ||= 0.00 
+    self.debit ||= 0.00
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
   has_one :balance
   attr_accessible :email, :name
+
+  after_initialize do
+    self.balance ||= self.build_balance
+  end
 end
