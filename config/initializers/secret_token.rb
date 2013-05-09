941b6428beeefd5a3100a82fc9eb82b4af958f2f
Sherlock Yao <yaosherlock@gmail.com>
2013-05-09 12:24:47 +0800

    initializ project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FeedMe::Application.config.secret_token = 'd7cc0459ef2af7dc453960bd2f12d26a27ffb7aff81d43534bf9fe9e645fe48f27975678a218983aa979d3ccc57d5654d67b66aa31c10119c4eb1161dacb6604'
