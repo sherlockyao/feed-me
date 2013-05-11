d9b9f77247f7d0d2d21e51e1f6d2fe16515aa06c
Sherlock Yao <yaosherlock@gmail.com>
2013-05-11 10:52:52 +0800

    add credit method for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,18 @@ class User < ActiveRecord::Base
   after_initialize do
     self.balance ||= self.build_balance
   end
+
+  def self.credit(amount, debitor_id)
+    debitor = User.find(debitor_id)
+    Cashflow.new(:amount => amount, :debitor => debitor, :creditor => self)
+    Balance.transaction do
+      self.balance.credit += amount
+      self.balance.save!
+      debitor.balance.with_lock do
+        debitor.balance.debit += amount
+        debitor.balance.save!
+      end
+    end
+  end
+
 end
